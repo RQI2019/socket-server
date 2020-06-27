7fe90eecc12a3ee83ab6d4a716e377567896020d
agtlohan <timesofplays@hotmail.com>
2020-06-26 21:59:01 -0500

    primer commit - rest server

---
+++ index.ts
@@ -0,0 +1,26 @@
+import Server from './classes/server';
+import { SERVER_PORT } from './global/environment';
+import router from './routes/router';
+
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+
+
+const servidor = new Server();
+
+
+// bodyParse
+servidor.app.use( bodyParser.urlencoded({ extended: true }));
+servidor.app.use( bodyParser.json() );
+
+// cors
+
+servidor.app.use( cors( { origin: true, credentials: true } ) );
+
+
+servidor.app.use( "/", router );
+
+servidor.start( () => {
+    console.log(`El servidor esta corriendo en el puerto ${SERVER_PORT}`);
+} );
